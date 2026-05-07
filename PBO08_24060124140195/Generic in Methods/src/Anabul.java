10f2d06e82fa852cf8fa88fbf57b5f2067c107c3
TahuAtauTempeh <akka.m4a@gmail.com>
2026-05-07 16:30:11 +0700

    idk

---
+++ PBO08_24060124140195/Generic in Methods/src/Anabul.java
@@ -0,0 +1,18 @@
+// File         : Anabul.java
+// Deskripsi    : supertype file
+// NIM / Nama   : 24060124140195 / Azka Aqylla Maulana
+// Tanggal      : 23.04.26
+
+public abstract class Anabul {
+    // attributes
+    String Nama;
+
+    // methods
+    Anabul(String nem){
+        Nama = nem;
+    }
+
+    public abstract void Gerak();
+
+    public abstract void Bersuara();
+}
---
+++ PBO08_24060124140195/Generic in Methods/src/Kucing.java
@@ -0,0 +1,25 @@
+// File         : Kucing.java
+// Deskripsi    : subclass of anabul
+// NIM / Nama   : 24060124140195 / Azka Aqylla Maulana
+// Tanggal      : 30.04.26
+
+public class Kucing extends Anabul{
+    // attributes
+    double bobot;
+
+    // methods
+    Kucing(String nem, double weight){
+        super(nem);
+        bobot = weight;
+    }
+
+    @Override
+    public void Gerak(){
+        System.out.println(Nama + " melata");
+    }
+    
+    @Override
+    public void Bersuara(){
+        System.out.println(Nama + " meong");
+    }
+}

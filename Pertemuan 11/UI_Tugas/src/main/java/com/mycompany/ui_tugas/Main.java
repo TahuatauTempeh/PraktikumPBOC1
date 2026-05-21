4bded05ddb15a66dccff1e5baa7702127de9c11f
TahuAtauTempeh <akka.m4a@gmail.com>
2026-05-21 16:47:11 +0700

    UI?

---
+++ Pertemuan 11/UI_Tugas/src/main/java/com/mycompany/ui_tugas/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ui_tugas;
+
+/**
+ *
+ * @author Akka
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
